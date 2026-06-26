efcec12968270e198b4b3f3556712eef0bdad9ba
Ajay <ajaykandula1006@gmail.com>
2026-06-26 23:33:38 +0530

    Adding LeetCode Problems:Lucky Number, TwoSum

---
+++ src/LeetCode/LuckyNumber.java
@@ -0,0 +1,66 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LuckyNumber {
+    public List<Integer> luckyNumber(int[][] mat){
+        for(int i=0;i< mat.length;i++) {
+            int low = mat[i][0];
+            int col = 0;
+            for (int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j] < low) {
+                    low = mat[i][j];
+                    col = j;
+                }
+            }
+            int max=mat[0][col];
+
+            for (int k = 0; k < mat.length; k++) {
+                    if (mat[k][col] > max){
+                        max = mat[k][col];
+
+                    }
+            }
+            if(low  == max )return List.of(max);
+        }
+        return List.of();
+    }
+
+    public static void printMatrix(int[][] mat){
+        System.out.print("Matrix: ");
+        for(int i=0;i<mat.length;i++){
+            System.out.print("{");
+            for(int j=0;j<mat[i].length;j++){
+                System.out.print(mat[i][j]);
+                if(j < mat[i].length-1){
+                    System.out.print(",");
+                }
+            }
+            System.out.print("} ");
+        }
+        System.out.println();
+    }
+
+    public static void printResult(int[][] arr){
+        System.out.print("Given ");
+        printMatrix(arr);
+        LuckyNumber obj=new LuckyNumber();
+        List<Integer> result=obj.luckyNumber(arr);
+        System.out.println("Lucky numbers in the Given Matrix: " + result + " ");
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{3,7,8},{9,11,13},{15,16,17}};
+        int[][]  matrix2 = {{1,10,4,2},{9,3,8,7},{15,16,17,12}};
+        int[][] matrix3= {{7,8},{1,2}};
+
+
+
+        printResult(matrix);
+        printResult(matrix2);
+        printResult(matrix3);
+    }
+}
---
+++ src/LeetCode/TwoSum.java
@@ -0,0 +1,52 @@
+package LeetCode;
+
+public class TwoSum {
+        public static int[] twoSum(int[] nums,int target){
+        for(int i=0;i<nums.length - 1;i++){
+         for(int j=i+1;j<nums.length;j++) {
+            if (nums[i] + nums[j] == target) {
+                return new int[]{i,j};
+        }
+    }
+        }
+        return new int[]{};
+}
+
+public static void printArray(int[] arr){
+        System.out.print("Array: ");
+        System.out.print("{");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+            if(i <arr.length-1){
+                System.out.print(",");
+            }
+        }
+        System.out.print("} ");
+        System.out.println();
+    }
+
+    public static void printResult(int[] arr,int target){
+        printArray(arr);
+        System.out.println("target: " + target);
+        System.out.print("Result ");
+        int[] result=twoSum(arr,target);
+        printArray(result);
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums={2,7,11,15};
+        int target=9;
+
+        int[] nums2={3,2,4};
+        int target2=6;
+
+        int[] nums3={3,3};
+        int target3=6;
+
+        printResult(nums,target);
+        printResult(nums2,target2);
+        printResult(nums3,target3);
+    }
+}
